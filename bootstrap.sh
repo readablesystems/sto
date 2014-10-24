ea6baa4033d9ec05609c233e2df695d2197d4c36
Eddie Kohler <ekohler@gmail.com>
2014-10-24 09:56:22 -0400

    Masstree fix.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
 
+if [ ! -f masstree-beta/configure.ac ]; then
+    git submodule init
+    git submodule update
+fi
+
+if [ -x masstree-beta/bootstrap.sh ]; then
+    ( cd masstree-beta; ./bootstrap.sh )
+fi
+
 autoreconf -i
