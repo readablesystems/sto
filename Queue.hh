be75bb9b24330dcb570fe6f3a90f07bf8299a797
Lillian Tsai <tslilyai@localhost.localdomain>
2015-02-19 08:57:57 -0500

    fixed bugs in queue, total-search reference not found?

--- Queue.hh
+++ Queue.hh
@@ -1,3 +1,6 @@
+// atomic check_empty -- check empty, check version, check empty again
+// tailversion -- lock instead of tail, check version# if empty
+
 #pragma once
 
 #include <list>
@@ -8,24 +11,24 @@
 template <typename T, unsigned BUF_SIZE = 256> 
 class Queue: public Shared {
 public:
-    Queue() : head_(NULL), tail_(NULL), queuesize_(0), headversion_(0) {}
+    Queue() : head_(0), tail_(0), queuesize_(0), tailversion_(0), headversion_(0) {}
 
     typedef uint32_t Version;
     typedef VersionFunctions<Version, 0> QueueVersioning;
     
-    static constexpr Version lock_bit = 1<<1;
     static constexpr Version delete_bit = 1<<0;
 
-    T queueSlots[BUF_SIZE];
-
-//copy??????? reference
     void transPush(Transaction& t, const T& v) {
         auto& item = t.item(this, -1);
-        std::list<T> write_list;
-        if (item.has_write())
-            write_list = unpack<std::list<T>>(item.key());
-        write_list.push_back(v);
-        t.add_write(item, write_list);
+        if (item.has_write()) {
+            auto& write_list = unpack<std::list<T>>(item.key());
+            write_list.push_back(v);
+        }
+        else {
+            std::list<T> write_list;
+            write_list.push_back(v);
+            t.add_write(item, write_list);
+        }
     }
 
     void transPop(Transaction& t) {
@@ -34,11 +37,10 @@ public:
             if has_write(t.item(this,-1)) then remove first item off list (need to check that queue is still empty at commit time?)
             else fail
         */
-        std::ptrdiff_t index = get_index(head_);
-        std::ptrdiff_t tail_index = get_index(tail_);
+        unsigned index = head_;
         auto& item = t.item(this, index);
         while (has_delete(item)) {
-            if (index == tail_index);
+        //    if (index == tail_);
                 //assert fail; 
             item = t.item(this, index);
             index = (index + 1) % BUF_SIZE;
@@ -56,11 +58,10 @@ public:
             if has_write(t.item(this,-1)) then reference first item off list (need to check that queue is still empty at commit time?)
             else fail
         */
-        std::ptrdiff_t index = get_index(head_);
-        std::ptrdiff_t tail_index = get_index(tail_);
+        unsigned index = head_;
         auto& item = t.item(this, index);
         while (has_delete(item)) {
-            if (index == tail_index)
+         //   if (index == tail_)
             item = t.item(this, index);
             index = (index + 1) % BUF_SIZE;
         }
@@ -70,11 +71,6 @@ public:
     }
     
 private:
-    std::ptrdiff_t get_index(T *ptr) {
-        if (ptr) return ptr - queueSlots;
-        else return 0;
-    }
-    
     bool has_delete(TransItem& item) {
         return item.has_flags(delete_bit);
     }
@@ -91,50 +87,48 @@ private:
         if (has_delete(item))
             lock(headversion_);
         else if (item.has_write())
-            tail_.atomic_add_flags(lock_bit);
+            lock(tailversion_);
     }
 
     void unlock(TransItem& item) {
         if (has_delete(item)) 
             unlock(headversion_);
         else if (item.has_write())
-            tail_.set_flags(tail_.flags() & ~lock_bit);
+            unlock(tailversion_);
     }
   
     bool check(TransItem& item, Transaction& t) {
 	    //if transaction assumed queue was empty, check if still empty
+        (void) t;
         auto hv = headversion_;
-        //check to ensure that no other thread has locked head/tail, or that we were the ones to lock
-        // only need to check if another transaction is about to change the version number if this is a read???? do we even need to check for intertransactional locks because we don't update queueversion if tail is modified (i.e. only pushes occur)??
         return (QueueVersioning::versionCheck(hv, item.template read_value<Version>()) && (!QueueVersioning::is_locked(hv) || !has_delete(item)));
     }
 
     void install(TransItem& item) {
 	    if (has_delete(item)) {
-            auto index = get_index(head_);
-            assert(item.key() == (void*)index);
-            head_ = &queueSlots[index+1 % BUF_SIZE];
+            head_ = head_+1 % BUF_SIZE;
             QueueVersioning::inc_version(headversion_);
         }
-        // another transaction inserting onto tail = don't need to increment queueversion????
+        // another transaction inserting onto tail = don't need to increment queueversion
         else {
             auto write_list = unpack<std::list<T>>(item.key());
-            auto head_index = get_index(head_);
-            auto index = get_index(tail_);
+            auto head_index = head_;
             while (!write_list.empty()) {
-                if (index != head_index) {
-                    index = (index+1) % BUF_SIZE;
-                    queueSlots[index] = write_list.front();
+                if (tail_ != head_index) {
+                    tail_ = (tail_ +1) % BUF_SIZE;
+                    queueSlots[tail_] = write_list.front();
                     write_list.pop_front();
                 }
                 //else return "fail"; ?????
             }
-            tail_ = &queueSlots[index % BUF_SIZE];
         }
     }
+    
+    T queueSlots[BUF_SIZE];
 
-    T* head_;
-    TaggedLow<T> tail_;
-    long queuesize_;
+    unsigned head_;
+    unsigned tail_;
+    unsigned queuesize_;
+    Version tailversion_;
     Version headversion_;
 };
