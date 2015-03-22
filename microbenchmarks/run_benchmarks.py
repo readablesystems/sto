6eb3bb901b2602137a1e8c93bada4c01948660f3
Yihe Huang <yihehuang@g.harvard.edu>
2015-03-22 18:09:13 -0400

    skipping txlen=100 during scaling tests

--- microbenchmarks/run_benchmarks.py
+++ microbenchmarks/run_benchmarks.py
@@ -7,7 +7,7 @@ bm_prog = "concurrent"
 bm_exec = bm_dir + bm_prog
 
 opacity_names = ["no opacity", "TL2 opacity", "slow opacity"]
-scaling_txlens = [5, 10, 25, 50, 75, 100, 150, 200, 250, 300]
+scaling_txlens = [5, 10, 25, 50, 75, 150, 200, 250, 300]
 fixed_txlen = 100
 nthreads_max = multiprocessing.cpu_count()
 
