eb59117e62c56f433807f8234280f7d5bdd0b7e3
Nathaniel Herman <nherman@college.harvard.edu>
2015-03-25 23:52:27 -0400

    remove commented out lines

--- GenericSTM.hh
+++ GenericSTM.hh
@@ -13,14 +13,12 @@ public:
 
     auto it = t.check_item(this, word);
     if (it && it->has_write()) {
-        //t.check_reads();
         return it->template write_value<T>();
     }
     
     size_t key = bucket(word);
     // ensures version doesn't change
     table_.transRead(t, key);
-    //t.check_reads();
     T ret = *word;
     return ret;
   }
@@ -33,7 +31,6 @@ public:
     size_t key = bucket(word);
     table_.transWrite(t, key, 0);
     t.item(this, word).add_write(new_val).assign_flags(sizeof(T) << TransItem::userf_shift);
-    //t.check_reads();
   }
 
   // Hashtable handles all of this
