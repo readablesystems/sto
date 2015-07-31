8f6415a627a66291e066ac2be3aa27c112003ad2
jinala <jinala@mit.edu>
2015-07-30 23:43:26 -0400

    Transactional iterator for array along with tests.

--- Array1.hh
+++ Array1.hh
@@ -7,8 +7,13 @@
 #include "SingleElem.hh"
 #include "VersionFunctions.hh"
 
+template<typename T, unsigned N, typename Elem = SingleElem<T>> class Array1Iter;
+
 template <typename T, unsigned N, typename Elem = SingleElem<T>>
 class Array1 : public Shared {
+    friend class Array1Iter<T, N, Elem>;
+    typedef Array1Iter<T, N, Elem> iterator;
+    
     typedef uint32_t Version;
     typedef VersionFunctions<Version> Versioning;
   public:
@@ -55,7 +60,51 @@ class Array1 : public Shared {
         //install value
         data_[item.key<key_type>()].install(item, t);
     }
-
+    
+    iterator begin() { return iterator(this, 0); }
+    iterator end() { return iterator(this, N); }
+    
   private:
     Elem data_[N];
 };
+
+template<typename T, unsigned N, typename Elem>
+class Array1Iter : public std::iterator<std::forward_iterator_tag, T> {
+public:
+    typedef T value_type;
+    Array1Iter(Array1<T, N, Elem> * arr, int ptr) : myArr(arr), myPtr(ptr) { }
+    Array1Iter(const Array1Iter& itr) : myArr(itr.myArr), myPtr(itr.myPtr) {}
+    
+    Array1Iter& operator= (const Array1Iter& v) {
+        myArr = v.myArr;
+        myPtr = v.myPtr;
+        return *this;
+    }
+    
+    bool operator==(Array1Iter<T, N, Elem> other) const {
+        return (myArr == other.myArr) && (myPtr == other.myPtr);
+    }
+    
+    bool operator!=(Array1Iter<T, N, Elem> other) const {
+        return !(operator==(other));
+    }
+    
+    Elem& operator*() {
+        return myArr->data_[myPtr];
+    }
+    
+    /* This is the prefix case */
+    Array1Iter<T, N, Elem>& operator++() { ++myPtr; return *this; }
+    
+    /* This is the postfix case */
+    Array1Iter<T, N, Elem> operator++(int) {
+        Array1Iter<T, N, Elem> clone(*this);
+        ++myPtr;
+        return clone;
+    }
+    
+private:
+    Array1<T, N, Elem> * myArr;
+    int myPtr;
+};
+
--- array1.cc
+++ array1.cc
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <assert.h>
+#include <vector>
 #include "Transaction.hh"
 #include "Array1.hh"
 
@@ -38,8 +39,155 @@ void testSimpleString() {
 	printf("PASS: testSimpleString\n");
 }
 
+void testIter() {
+    std::vector<int> arr;
+    Array1<int, 10> f;
+    for (int i = 0; i < 10; i++) {
+        int x = rand();
+        arr.push_back(x);
+        f.write(i, x);
+    }
+    int max;
+    TRANSACTION {
+        max = *(std::max_element(f.begin(), f.end()));
+    } RETRY(false);
+    
+    assert(max == *(std::max_element(arr.begin(), arr.end())));
+    printf("Max is %i\n", max);
+    printf("PASS: array max_element test\n");
+    
+}
+
+
+void testConflictingIter() {
+    Array1<int, 10> f;
+    for (int i = 0; i < 10; i++) {
+        f.write(i, i);
+    }
+
+    Transaction t;
+    Sto::set_transaction(&t);
+    std::max_element(f.begin(), f.end());
+    
+    Transaction t1;
+    Sto::set_transaction(&t1);
+    f.transWrite(4, 10);
+    assert(t1.try_commit());
+    assert(!t.try_commit());
+    printf("PASS: conflicting array max_element test\n");
+    
+}
+
+void testModifyingIter() {
+    Array1<int, 10> f;
+    for (int i = 0; i < 10; i++) {
+        f.write(i, i);
+    }
+    
+    Transaction t;
+    Sto::set_transaction(&t);
+    std::replace(f.begin(), f.end(), 4, 6);
+    assert(t.try_commit());
+    
+    
+    Transaction t1;
+    Sto::set_transaction(&t1);
+    int v = f.transRead(4);
+    assert(t1.try_commit());
+    
+    assert(v == 6);
+    printf("PASS: array replace test\n");
+}
+
+void testConflictingModifyIter1() {
+    Array1<int, 10> f;
+    for (int i = 0; i < 10; i++) {
+        f.write(i, i);
+    }
+    
+    Transaction t;
+    Sto::set_transaction(&t);
+    std::replace(f.begin(), f.end(), 4, 6);
+    
+    Transaction t1;
+    Sto::set_transaction(&t1);
+    f.transWrite(4, 10);
+    assert(t1.try_commit());
+    
+    assert(!t.try_commit());
+    
+    Transaction t2;
+    Sto::set_transaction(&t2);
+    int v = f.transRead(4);
+    assert(t2.try_commit());
+    
+    assert(v == 10);
+    
+    printf("PASS: array conflicting replace test1\n");
+}
+
+void testConflictingModifyIter2() {
+    Array1<int, 10> f;
+    for (int i = 0; i < 10; i++) {
+        f.write(i, i);
+    }
+    
+    Transaction t;
+    Sto::set_transaction(&t);
+    std::replace(f.begin(), f.end(), 4, 6);
+    assert(t.try_commit());
+    
+    Transaction t1;
+    Sto::set_transaction(&t1);
+    f.transWrite(4, 10);
+    assert(t1.try_commit());
+    
+    Transaction t2;
+    Sto::set_transaction(&t2);
+    int v = f.transRead(4);
+    assert(t2.try_commit());
+    
+    assert(v == 10);
+    
+    printf("PASS: array conflicting replace test2\n");
+}
+
+void testConflictingModifyIter3() {
+    Array1<int, 10> f;
+    for (int i = 0; i < 10; i++) {
+        f.write(i, i);
+    }
+    
+    Transaction t1;
+    Sto::set_transaction(&t1);
+    f.transRead(4);
+    
+    Transaction t;
+    Sto::set_transaction(&t);
+    std::replace(f.begin(), f.end(), 4, 6);
+    assert(t.try_commit());
+    
+    assert(!t1.try_commit());
+    
+    Transaction t2;
+    Sto::set_transaction(&t2);
+    int v = f.transRead(4);
+    assert(t2.try_commit());
+    
+    assert(v == 6);
+    
+    printf("PASS: array conflicting replace test3\n");
+}
+
+
 int main() {
 	testSimpleInt();
 	testSimpleString();
+    testIter();
+    testConflictingIter();
+    testModifyingIter();
+    testConflictingModifyIter1();
+    testConflictingModifyIter2();
+    testConflictingModifyIter3();
 	return 0;
 }
