6e645b671bd7840310098e53b408aa068befe2d1
Nathaniel Herman <nherman@post.harvard.edu>
2016-02-09 01:12:24 -0800

    Add an (untested) boosted hashtable. Still need to get the inverses working as C functions (I think some weird virtual thing might do the trick).

--- Boosting.hh
+++ Boosting.hh
@@ -184,3 +184,114 @@ void releaseLocksCallback(void*, void*) {
   rwlockset.clear();
 }
 
+#define READ_SPIN 100
+#define WRITE_SPIN 100
+
+#define DO_ABORT() TxAbort(Self)
+
+#define ON_ABORT(callback, context1, context2) TxAbortHook(Self, (callback), (context1), (context2))
+
+template <typename K, unsigned Init_size = 129, typename Hash = std::hash<K>, typename Pred = std::equal_to<K>>>
+class LockKey {
+public:
+  LockKey(unsigned size = Init_size, Hash h = Hash(), Pred p = Pred()) : lockMap(size, h, p) {}
+
+  void readLock(const K& key) {
+    RWLock *lock = getLock(key);
+    std::tie(std::ignore, inserted) = rwlockset.insert(lock);
+    // check if we already have the lock or need to get it now
+    if (inserted) {
+      if (!lock.tryReadLock(READ_SPIN)) {
+        rwlockset.erase(lock);
+        DO_ABORT();
+      }
+    }
+  }
+
+  void writeLock(const K& key) {
+    RWLock *lock = getLock(key);
+    std::tie(std::ignore, inserted) = rwlockset.insert(lock);
+    if (inserted) {
+      // don't have the lock in any form yet
+      if (!lock.tryWriteLock(WRITE_SPIN)) {
+        rwlockset.erase(lock);
+        DO_ABORT();
+      }
+      return;
+    }
+    // only have a read lock so far
+    if (!lock.isWriteLocked()) {
+      if (!lock.tryUpgrade(WRITE_SPIN)) {
+        DO_ABORT();
+      }
+    }
+  }
+
+private:
+  Hashtable<K, RWLock*, true, Init_size, RWLock*, Hash, Pred> lockMap;
+
+  RWLock *getLock(const K& key) {
+    RWLock *lock;
+    if (!lockMap.read(key, lock)) {
+      // TODO(nate): might want to acquire the lock before we insert it
+      lock = new RWLock();
+      // lock will either stay the same or be set to the current lock if one exists.
+      lockMap.putIfAbsent(key, lock);
+    }
+    return lock;
+  }
+};
+
+template <typename K, typename V, unsigned Init_size = 129, typename Hash = std::hash<K>, typename Pred = std::equal_to<K>>
+class TransHashtable {
+  Hashtable<K, V, true, Init_size, V, Hash, Pred> hashtable;
+  LockKey<K, Init_size, Hash, Pred> lockKey;
+
+public:
+  typedef K Key;
+  typedef V Value;
+
+  TransHashtable(unsigned size = Init_size, Hash h = Hash(), Pred p = Pred()) : hashtable(size, h, p), lockKey(size, h, p) {}
+
+  bool transGet(const Key& k, Value& retval) {
+    lockKey.readLock(k);
+    return hashtable.read(k, retval);
+  }
+
+  virtual void _undoDelete(std::pair<Key, Value> *pair) {
+    hashtable.put(pair.key, pair.value);
+    delete pair;
+  }
+
+  virtual void _undoInsert(Key *key) {
+    hashtable.remove(key);
+    delete key;
+  }
+
+  bool transDelete(const Key& k) {
+    lockKey.writeLock(k);
+    Value oldval;
+    if (!hashtable.read(k, oldval)) {
+      return false;
+    }
+    bool success = hashtable.remove(k);
+    assert(success);
+
+    auto *pair = new std::pair<Key, Value>(k, std::move(oldval));
+
+    //    ON_ABORT(&TransHashtable::_undoDelete, this, pair);
+
+    return success;
+  }
+
+  bool transInsert(const Key& k, const Value& val) {
+    lockKey.writeLock(k);
+    bool success = hashtable.insert(k, val);
+    
+    if (success) {
+      //    ON_ABORT(Hashtable::remove, this, k);
+    }
+
+    return success;
+  }
+};
