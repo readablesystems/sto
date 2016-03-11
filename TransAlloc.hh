e5cdb43c7d9aef16f0ddc68a3089254e1719f2f9
Nathaniel Herman <nherman@post.harvard.edu>
2016-03-10 23:29:56 -0800

    add transDelete and transNew for safe nontrivial object allocation/free (by a user) in a transaction. transNew isn't really safe right now

--- TransAlloc.hh
+++ TransAlloc.hh
@@ -6,7 +6,7 @@
 
 class TransAlloc : public Shared {
 public:
-    enum { alloc_flag = 1 };
+    static constexpr TransItem::flags_type alloc_flag = TransItem::user0_bit;
 
     // used to free things only if successful commit
     void transFree(void *ptr) {
@@ -16,22 +16,47 @@ public:
     // malloc() which will be freed on abort
     void* transMalloc(size_t sz) {
         void *ptr = malloc(sz);
-        Sto::new_item(this, ptr).add_write(int(alloc_flag));
+        Sto::new_item(this, ptr).add_write(0).add_flags(alloc_flag);
         return ptr;
+    }
+
+    // delete which only applies if transaction commits
+    template <typename T>
+    void transDelete(T *x) {
+        Sto::new_item(this, x).add_write(&ObjectDestroyer<T>::destroy_and_free);
+    }
+
+    // new which will be delete'd on abort.
+    // arguments go to T's constructor
+    template <typename T, typename... Args>
+    T* transNew(Args&&... args) {
+        T* ret = new T(std::forward<Args>(args)...);
+        Sto::new_item(this, ret).add_write(&ObjectDestroyer<T>::destroy_and_free).add_flags(alloc_flag);
     }
 
     bool lock(TransItem&, Transaction&) { return true; }
     bool check(const TransItem&, const Transaction&) { assert(0); return false; }
     void install(TransItem& item, const Transaction&) {
-        if (item.write_value<int>() != alloc_flag) {
+        if (item.has_flag(alloc_flag))
+            return;
+        if (item.write_value<int>() != 0) {
+            auto func = item.write_value<void(*)(void*)>();
+            Transaction::rcu_call(func, item.key<void*>());
+        } else {
             void *ptr = item.key<void*>();
             Transaction::rcu_free(ptr);
         }
     }
     void unlock(TransItem&) {}
     void cleanup(TransItem& item, bool committed) {
-        if (!committed && item.write_value<int>() == alloc_flag)
+        if (committed || !item.has_flag(alloc_flag))
+            return;
+        if (item.write_value<int>() != 0) {
+            auto func = item.write_value<void(*)(void*)>();
+            Transaction::rcu_call(func, item.key<void*>());
+        } else {
             Transaction::rcu_free(item.key<void*>());
+        }
     }
     void print(std::ostream& w, const TransItem& item) const {
         w << "{TransAlloc @" << item.key<void*>();
