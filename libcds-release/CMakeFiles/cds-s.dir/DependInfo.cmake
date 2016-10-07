0545ad0c23fb271983764c1cdadf567ecdb51667
Lily Tsai <tslilyai@mat.lcs.mit.edu>
2016-10-06 22:00:56 -0400

    add librelease dir

---
+++ libcds-release/CMakeFiles/cds-s.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tslilyai/Documents/Research/sto/libcds/src/dhp_gc.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/dhp_gc.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/dllmain.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/dllmain.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/hp_gc.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/hp_gc.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/init.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/init.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/michael_heap.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/michael_heap.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/topology_hpux.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/topology_hpux.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/topology_linux.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/topology_linux.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/topology_osx.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/topology_osx.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/urcu_gp.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/urcu_gp.cpp.o"
+  "/home/tslilyai/Documents/Research/sto/libcds/src/urcu_sh.cpp" "/home/tslilyai/Documents/Research/sto/libcds-release/CMakeFiles/cds-s.dir/src/urcu_sh.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/home/tslilyai/Documents/Research/sto/libcds"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ libcds-release/CMakeFiles/cds.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cds.dir/src/hp_gc.cpp.o"
+  "CMakeFiles/cds.dir/src/init.cpp.o"
+  "CMakeFiles/cds.dir/src/dhp_gc.cpp.o"
+  "CMakeFiles/cds.dir/src/urcu_gp.cpp.o"
+  "CMakeFiles/cds.dir/src/urcu_sh.cpp.o"
+  "CMakeFiles/cds.dir/src/michael_heap.cpp.o"
+  "CMakeFiles/cds.dir/src/topology_hpux.cpp.o"
+  "CMakeFiles/cds.dir/src/topology_linux.cpp.o"
+  "CMakeFiles/cds.dir/src/topology_osx.cpp.o"
+  "CMakeFiles/cds.dir/src/dllmain.cpp.o"
+  "bin/libcds.pdb"
+  "bin/libcds.so"
+  "bin/libcds.so.2.2.0"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
