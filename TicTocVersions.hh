0f52a91a4333284fbe8fcfcc058ad3c09fb5d943
Yihe Huang <yihehuang@g.harvard.edu>
2016-10-07 18:04:16 -0400

    implementing compound tictoc

--- TicTocVersions.hh
+++ TicTocVersions.hh
@@ -1,6 +1,8 @@
 #pragma once
 #include "Interface.hh"
 
+#define TICTOC_IMPL_COMPOUND 1
+
 // Transaction timestamp (TID) as described in the TicToc concurrency control paper
 
 class LockableTid {
@@ -12,6 +14,7 @@ public:
     static constexpr type lock_bit = type(0x1<<5);      // bit 5
     static constexpr type nonopaque_bit = type(0x1<<6); // bit 6
     static constexpr type user_bit = type(0x1<<7);      // bit 7
+    static constexpr type lockable_mask = type(0xff);
 
     // MEAT
     type t_;
@@ -89,6 +92,21 @@ public:
     bool is_nonopaque() const {
         return t_ & nonopaque_bit;
     }
+
+    friend std::ostream& operator<<(std::ostream& w, LockableTid t) {
+        auto f = w.flags();
+        auto& v = t.t_;
+        w << std::hex;
+        v &= lockable_mask;
+        if (v & ~(user_bit - 1))
+            w << "U" << (v & ~(user_bit - 1));
+        if (v & nonopaque_bit)
+            w << "!";
+        if (v & lock_bit)
+            w << "L" << std::dec << (v & (lock_bit - 1));
+        w.flags(f);
+        return w;
+    }
 };
 
 class RawTid : public LockableTid {
@@ -98,8 +116,9 @@ public:
 
     RawTid(type x) : LockableTid(x) {};
     RawTid() : LockableTid() {};
-    void operator=(const LockableTid& t) {
+    RawTid& operator=(const LockableTid& t) {
         t_ = t.t_;
+        return *this;
     }
     bool operator==(const RawTid& rhs) const {
         return t_ == rhs.t_;
@@ -167,135 +186,232 @@ public:
     static constexpr type delta_mask = type(0xff<<delta_shift);   // bits 8-15 hold "delta"
     static constexpr type increment_value = type(0x1<<wts_shift); // bits 16-63 hold wts
 
-    CompoundTid(uint64_t x) : t_(x) {};
-    CompoundTid() : t_() {};
+    CompoundTid(type x) : LockableTid(x) {};
+    CompoundTid() : LockableTid() {};
 
-    uint64_t wts_value() const;
-    uint64_t rts_value() const;
+    CompoundTid& operator=(const LockableTid& t) {
+        t_ = t.t_;
+        return *this;
+    }
+    bool operator==(const CompoundTid& rhs) const {
+        return t_ == rhs.t_;
+    }
+    bool operator!=(const CompoundTid& rhs) const {
+        return t_ != rhs.t_;
+    }
 
-    void set_timestamps(uint64_t commit_ts);
-    bool validate_timestamps(CompoundTid read_tss, uint64_t commit_ts);
-private:
-    type t_;
+    type wts_value() const;
+    type rts_value() const;
+
+    void set_timestamps(type commit_ts, type flags);
+    void set_timestamps_unlock(type commit_ts, type flags);
+    bool validate_timestamps(CompoundTid read_tss, type commit_ts);
 };
 
-typedef RawTid TicTocTid;
+typedef LockableTid TicTocTid;
 
 class TicTocVersion {
 public:
     typedef LockableTid::type type;
     static constexpr type user_bit = LockableTid::user_bit;
 
-    TicTocVersion() : wts_(), rts_() {}
-    TicTocVersion(const TicTocVersion& rhs) : wts_(rhs.wts_), rts_(rhs.rts_) {}
-    TicTocVersion(type ts) : wts_(ts), rts_(ts) {}
-    TicTocVersion(type wts, type rts) : wts_(wts), rts_(rts) {}
+    // constructors
+    TicTocVersion();
+    TicTocVersion(const TicTocVersion& rhs);
+    TicTocVersion(type ts);
 
-    bool operator==(const TicTocVersion& x) const {
-        return ((wts_ == x.wts_) && (rts_ == x.rts_));
-    }
-    bool operator!=(const TicTocVersion& x) const {
-        return ((wts_ != x.wts_) || (rts_ != x.rts_));
-    }
-    TicTocVersion& operator=(const TicTocVersion& rhs) {
-        wts_ = rhs.wts_;
-        fence(); // XXX the correct fence?
-        rts_ = rhs.rts_;
-        return *this;
-    }
-
-    LockableTid& get_lockable() {
-        return wts_;
-    }
+    // operators
+    bool operator==(const TicTocVersion& x) const;
+    bool operator!=(const TicTocVersion& x) const;
+    TicTocVersion& operator=(const TicTocVersion& rhs);
 
     inline type snapshot(const TransItem& item, const Transaction& txn);
     inline type snapshot(TransProxy& item);
 
-    bool is_locked() const {
-        return wts_.is_locked();
+    inline LockableTid& get_lockable();
+    inline const LockableTid& get_lockable() const;
+
+    // locking
+    bool is_locked() {
+        return get_lockable().is_locked();
     }
-    bool is_locked_here() const {
-        return wts_.is_locked_here();
+    bool is_locked_here() {
+        return get_lockable().is_locked_here();
     }
-    bool is_locked_here(int here) const {
-        return wts_.is_locked_here(here);
+    bool is_locked_here(int here) {
+        return get_lockable().is_locked_here(here);
     }
-    inline bool is_locked_here(const Transaction& txn) const;
     bool is_locked_elsewhere() const {
-        return wts_.is_locked_elsewhere();
+        return get_lockable().is_locked_elsewhere();
     }
     bool is_locked_elsewhere(int here) const {
-        return wts_.is_locked_elsewhere(here);
+        return get_lockable().is_locked_elsewhere(here);
     }
-    inline bool is_locked_elsewhere(const Transaction& txn) const;
     bool has_user_bit_set() const {
-        return wts_.has_user_bit_set();
+        return get_lockable().has_user_bit_set();
     }
     void set_user_bit() {
-        wts_.set_user_bit();
+        get_lockable().set_user_bit();
     }
     void clear_user_bit() {
-        wts_.clear_user_bit();
+        get_lockable().clear_user_bit();
     }
 
     bool try_lock() {
-        return wts_.try_lock();
+        return get_lockable().try_lock();
     }
     bool try_lock(int here) {
-        return wts_.try_lock(here);
+        return get_lockable().try_lock(here);
     }
     void lock() {
-        wts_.lock();
+        get_lockable().lock();
     }
     void lock(int here) {
-        wts_.lock(here);
+        get_lockable().lock(here);
     }
     void unlock() {
-        wts_.unlock();
+        get_lockable().unlock();
     }
     void unlock(int here) {
-        wts_.unlock(here);
-    }
-    TicTocVersion unlocked() const {
-        auto ts = *this;
-        acquire_fence();
-        ts.wts_ = ts.wts_.unlocked();
-        return ts;
+        get_lockable().unlock(here);
     }
 
-    type read_timestamp() const {
-        return rts_.timestamp();
-    }
-    type write_timestamp() const {
-        return wts_.timestamp();
-    }
+    inline bool is_locked_here(const Transaction& txn) const;
+    inline bool is_locked_elsewhere(const Transaction& txn) const;
+    inline TicTocVersion unlocked() const;
 
-    void set_timestamps(type commit_ts, type flags = 0) {
-        wts_.set_timestamp(commit_ts, flags);
-        rts_.set_read_timestamp(commit_ts, flags);
-    }
-    void set_timestamps_unlock(type commit_ts, type flags = 0) {
-        rts_.set_read_timestamp(commit_ts, flags);
-        wts_.set_timestamp_unlock(commit_ts, flags);
-    }
-    bool validate_timestamps(const TicTocVersion& old_tss, type commit_ts) {
-        return RawTid::validate_timestamps(wts_, rts_, old_tss.wts_, old_tss.rts_, commit_ts);
-    }
+    inline type read_timestamp() const;
+    inline type write_timestamp() const;
+
+    inline void set_timestamps(type commit_ts, type flags = 0);
+    inline void set_timestamps_unlock(type commit_ts, type flags = 0);
+    inline bool validate_timestamps(const TicTocVersion& old_tss, type commit_ts);
 
     friend std::ostream& operator<<(std::ostream& w, TicTocVersion v) {
-        w << "w:";
-        RawTid::print(v.wts_, w);
-        w << "r:";
-        RawTid::print(v.rts_, w);
+        LockableTid l = v.get_lockable();
+        w << l << " w:"
+            << v.write_timestamp() << "r:" << v.read_timestamp() << std::endl;
         return w;
     }
 
     template <typename Exception>
     static inline void opaque_throw(const Exception& exception);
 private:
+#if TICTOC_IMPL_COMPOUND == 1
+    CompoundTid tss_;
+#else
     RawTid wts_;
     RawTid rts_;
+#endif
 };
+
+// The guts of TicTocVersion depend on the actual underlying
+// implementation (compound or separate words)
+
+#if TICTOC_IMPL_COMPOUND == 1
+
+// Implementing TicTocVersion using a compound word: wts + delta
+
+TicTocVersion::TicTocVersion() : tss_() {}
+TicTocVersion::TicTocVersion(const TicTocVersion& rhs) : tss_(rhs.tss_) {}
+TicTocVersion::TicTocVersion(type ts) : tss_(ts) {}
+
+bool TicTocVersion::operator==(const TicTocVersion& x) const {
+    return tss_ == x.tss_;
+}
+bool TicTocVersion::operator!=(const TicTocVersion& x) const {
+    return tss_ != x.tss_;
+}
+TicTocVersion& TicTocVersion::operator=(const TicTocVersion& rhs) {
+    tss_ = rhs.tss_;
+    return *this;
+}
+
+LockableTid& TicTocVersion::get_lockable() {
+    return tss_;
+}
+const LockableTid& TicTocVersion::get_lockable() const {
+    return tss_;
+}
+
+TicTocVersion TicTocVersion::unlocked() const {
+    auto ts = *this;
+    acquire_fence();
+    ts.tss_ = ts.tss_.unlocked();
+    return ts;
+}
+
+TicTocVersion::type TicTocVersion::read_timestamp() const {
+    return tss_.rts_value();
+}
+TicTocVersion::type TicTocVersion::write_timestamp() const {
+    return tss_.wts_value();
+}
+
+void TicTocVersion::set_timestamps(type commit_ts, type flags) {
+    tss_.set_timestamps(commit_ts, flags);
+}
+void TicTocVersion::set_timestamps_unlock(type commit_ts, type flags) {
+    tss_.set_timestamps_unlock(commit_ts, flags);
+}
+bool TicTocVersion::validate_timestamps(const TicTocVersion& old_tss, type commit_ts) {
+    return tss_.validate_timestamps(old_tss.tss_, commit_ts);
+}
+
+#else
+
+// Implementing TicTocVersion as two separate words: wts ++ rts
+
+TicTocVersion::TicTocVersion() : wts_(), rts_() {}
+TicTocVersion::TicTocVersion(const TicTocVersion& rhs) : wts_(rhs.wts_), rts_(rhs.rts_) {}
+TicTocVersion::TicTocVersion(type ts) : wts_(ts), rts_(ts) {}
+
+bool TicTocVersion::operator==(const TicTocVersion& x) const {
+    return ((wts_ == x.wts_) && (rts_ == x.rts_));
+}
+bool TicTocVersion::operator!=(const TicTocVersion& x) const {
+    return ((wts_ != x.wts_) || (rts_ != x.rts_));
+}
+TicTocVersion& TicTocVersion::operator=(const TicTocVersion& rhs) {
+    wts_ = rhs.wts_;
+    fence(); // XXX the correct fence?
+    rts_ = rhs.rts_;
+    return *this;
+}
+
+LockableTid& TicTocVersion::get_lockable() {
+    return wts_;
+}
+const LockableTid& TicTocVersion::get_lockable() const {
+    return wts_;
+}
+
+TicTocVersion TicTocVersion::unlocked() const {
+    auto ts = *this;
+    acquire_fence();
+    ts.wts_ = ts.wts_.unlocked();
+    return ts;
+}
+
+TicTocVersion::type TicTocVersion::read_timestamp() const {
+    return rts_.timestamp();
+}
+TicTocVersion::type TicTocVersion::write_timestamp() const {
+    return wts_.timestamp();
+}
+
+void TicTocVersion::set_timestamps(type commit_ts, type flags) {
+    wts_.set_timestamp(commit_ts, flags);
+    rts_.set_read_timestamp(commit_ts, flags);
+}
+void TicTocVersion::set_timestamps_unlock(type commit_ts, type flags) {
+    rts_.set_read_timestamp(commit_ts, flags);
+    wts_.set_timestamp_unlock(commit_ts, flags);
+}
+bool TicTocVersion::validate_timestamps(const TicTocVersion& old_tss, type commit_ts) {
+    return RawTid::validate_timestamps(wts_, rts_, old_tss.wts_, old_tss.rts_, commit_ts);
+}
+#endif
 
 template <typename Exception>
 inline void TicTocVersion::opaque_throw(const Exception& exception) {
