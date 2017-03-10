52d63d33e567eff3b827f462ed39d7f832dcb055
Yihe Huang <yihehuang@g.harvard.edu>
2017-03-09 19:17:11 -0500

    implementing timing counters

--- SystemProfiler.hh
+++ SystemProfiler.hh
@@ -39,4 +39,48 @@ void profile(std::function<void(void)> profilee) {
     profile("perf", profilee);
 }
 
+// Profiling counters measuring run time breakdowns
+enum TimingCounters {
+    tc_commit = 0,
+    tc_validate,
+    tc_find_item,
+    tc_abort,
+    tc_cleanup,
+    tc_opacity,
+    tc_count
+};
+
+typedef uint64_t tc_counter_type;
+
+template <unsigned C, unsigned N, bool Less = (C < N)>
+struct tc_helper;
+
+template <unsigned C, unsigned N>
+struct tc_helper<C, N, true> {
+    static bool counter_exists(unsigned tc) {
+        return tc < N;
+    }
+    static void account_array(tc_counter_type *tcs, tc_counter_type v) {
+        tcs[C] += v;
+    }
+};
+
+template <unsigned C, unsigned N>
+struct tc_helper<C, N, false> {
+    static bool counter_exists(unsigned) { return false; }
+    static void account_array(tc_counter_type*, tc_counter_type) {}
+};
+
+struct tc_counters {
+    tc_counter_type tcs_[tc_count];
+    tc_counters() { reset(); }
+    tc_counter_type timing_counter(int name) {
+        return tc_helper<0, tc_count>::counter_exists(name) ? tcs_[name] : 0;
+    }
+    void reset() {
+        for (int i = 0; i < tc_count; ++i)
+            tcs_[i] = 0;
+    }
+};
+
 };
