23284e9e56c94529afe4cfed83e9871ba1459526
Yihe Huang <yihehuang@g.harvard.edu>
2017-03-29 14:34:10 -0400

    made profiler work with Silo

--- SystemProfiler.hh
+++ SystemProfiler.hh
@@ -14,6 +14,8 @@
 
 namespace Profiler {
 
+static constexpr auto perf_path = "/usr/bin/perf";
+
 void profile(const std::string& name, std::function<void(void)> profilee) {
     std::stringstream ss;
     ss << getpid();
@@ -23,10 +25,14 @@ void profile(const std::string& name, std::function<void(void)> profilee) {
     pid_t pid = fork();
     if (pid == 0) {
         int console = open("/dev/null", O_RDWR);
+        int rc;
+        (void)rc;
         assert(console > 0);
-        assert(dup2(console, STDOUT_FILENO) != -1);
-        assert(dup2(console, STDERR_FILENO) != -1);
-        exit(execl("/usr/bin/perf", "perf", "record", "-o", profile_name.c_str(), "-p", ss.str().c_str(), nullptr));
+        rc = dup2(console, STDOUT_FILENO);
+        assert(rc != -1);
+        rc = dup2(console, STDERR_FILENO);
+        assert(rc != -1);
+        exit(execl(perf_path, "perf", "record", "-o", profile_name.c_str(), "-p", ss.str().c_str(), nullptr));
     }
 
     profilee();
@@ -39,4 +45,56 @@ void profile(std::function<void(void)> profilee) {
     profile("perf", profilee);
 }
 
+class PerfProfiler {
+public:
+    PerfProfiler() : running(false), child_pid(0) {}
+
+    void start() {
+        if (running) {
+            std::cerr << "Profiler already running" << std::endl;
+            return;
+        }
+
+        std::stringstream spid;
+        spid << getpid();
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            std::cerr << "Error: cannot start profiler" << std::endl;
+            return;
+        }
+        if (pid == 0) {
+            int redir = open("/dev/null", O_RDWR);
+            assert(redir > 0);
+            int rc;
+            rc = dup2(redir, STDOUT_FILENO);
+            assert(rc != -1);
+            rc = dup2(redir, STDERR_FILENO);
+            assert(rc != -1);
+            rc = execl(perf_path, "perf", "record", "-o", "perf.data", "-p", spid.str().c_str(), nullptr);
+            exit(rc);
+        } else {
+            running = true;
+            child_pid = pid;
+        }
+    }
+
+    void stop() {
+        if (!running) {
+            std::cerr << "Profiler not running" << std::endl;
+            return;
+        }
+
+        kill(child_pid, SIGINT);
+        waitpid(child_pid, nullptr, 0);
+
+        running = false;
+        child_pid = 0;
+    }
+
+private:
+    bool running;
+    pid_t child_pid;
 };
+
+}
