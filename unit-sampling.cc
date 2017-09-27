62ea17d6ed7c5e4d8b004e48a379cf2bbdeec1c2
Yihe Huang <yihehuang@g.harvard.edu>
2017-09-27 16:41:47 -0400

    fix unit-sampling

--- unit-sampling.cc
+++ unit-sampling.cc
@@ -4,7 +4,8 @@
 using namespace StoSampling;
 
 int main() {
-    StoRandomDistribution *dist = new StoZipfDistribution(1000);
+    // testing a zipf distribution, generating numbers between 1 and 1000 with theta=0.8
+    StoRandomDistribution *dist = new StoZipfDistribution(1 /*seed*/, 1 /*low*/, 1000 /*high*/, 0.8 /*skew*/);
 
     for (int i = 0; i < 1000; ++i)
         std::cout << dist->sample() << ", ";
