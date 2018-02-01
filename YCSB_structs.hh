59ee55e5935fbe4bba004ec27e87deb3747a0e91
Hao Bai <haobai@Haos-iMac.local>
2018-02-01 17:38:40 -0500

    YCSB code, doesn't compile yet

--- YCSB_structs.hh
+++ YCSB_structs.hh
@@ -1,78 +1,19 @@
 #pragma once
 
+#include <string>
+#include <random>
+
+#include "TPCC_structs.hh" // tpcc::fix_string
 #include "str.hh" // lcdf::Str
 
 namespace ycsb {
 
 enum class mode_id : int { ReadOnly = 0, MediumContention, HighContention };
 
-template <size_t FL>
-class fix_string {
-public:
-    fix_string() {
-        memset(s_, ' ', FL);
-    }
-    fix_string(const char *c_str) {
-        memset(s_, ' ', FL);
-        strncpy(s_, c_str, FL);
-    }
-    fix_string(const std::string& str) {
-        memset(s_, ' ', FL);
-        strncpy(s_, str.c_str(), FL);
-    }
-
-    bool operator==(const char *c_str) const {
-        return strlen(c_str) == FL && !memcmp(s_, c_str, FL);
-    }
-    bool operator==(const fix_string& rhs) const {
-        return !memcmp(s_, rhs.s_, FL);
-    }
-    char operator[](size_t idx) const {
-        return s_[idx];
-    }
-    fix_string& operator=(const fix_string& rhs) {
-        initialize_from(rhs);
-        return *this;
-    }
-    fix_string& operator=(const fix_string& rhs) volatile {
-        initialize_from(rhs);
-        return const_cast<fix_string&>(*this);
-    }
-    explicit operator std::string() {
-        return std::string(s_, FL);
-    }
-
-    void insert_left(const char *buf, size_t cnt) {
-        memmove(s_ + cnt, s_, FL - cnt);
-        memcpy(s_, buf, cnt);
-    }
-
-    friend ::std::hash<fix_string>;
-
-private:
-    void initialize_from(const char *c_str) {
-        memset(s_, ' ', FL);
-        size_t len = strlen(c_str);
-        for (size_t i = 0; i < len && i < FL; ++i)
-            s_[i] = c_str[i];
-    }
-    void initialize_from(const std::string& str) {
-        initialize_from(str);
-    }
-    void initialize_from(const fix_string& fstr) {
-        memcpy(s_, fstr.s_, FL);
-    }
-    void initialize_from(const fix_string& fstr) volatile {
-        memcpy(const_cast<char *>(s_), fstr.s_, FL);
-    }
-
-    char s_[FL];
-};
-
 struct ycsb_key {
     ycsb_key(uint64_t id) {
-    	// no scan operations for ycsb,
-    	// so byte swap is not required.
+        // no scan operations for ycsb,
+        // so byte swap is not required.
         w_id = id;
     }
     bool operator==(const ycsb_key& other) const {
@@ -89,16 +30,29 @@ struct ycsb_key {
 };
 
 struct ycsb_value {
-    fix_string<100>  col0;
-    fix_string<100>  col1;
-    fix_string<100>  col2;
-    fix_string<100>  col3;
-    fix_string<100>  col4;
-    fix_string<100>  col5;
-    fix_string<100>  col6;
-    fix_string<100>  col7;
-    fix_string<100>  col8;
-    fix_string<100>  col9;
+    tpcc::fix_string<100>  cols[10];
+
+    static ycsb_value random_ycsb_value() {
+        ycsb_value ret;
+        for (int i = 0; i < 10; i++) {
+            cols[i] = fix_string<100>(random_a_string);
+        } 
+    }
+
+private:
+    static std::string random_a_string() {
+        std::string str(100, ' ');
+        for (auto i = 0u; i < len; ++i) {
+            auto n = dis(gen);
+            char c = (n < 26) ? char('a' + n) :
+                     ((n < 52) ? char('A' + (n - 26)) : char('0' + (n - 52)));
+            str[i] = c;
+        }
+        return str;
+    }
+
+    static std::mt19937 gen;
+    static std::uniform_int_distribution<> dis(0, 61);
 };
 
 }; // namespace ycsb
