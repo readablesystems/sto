bfa6b7e422798eb3a6981d1ef4c919c881fdbff4
Yihe Huang <yihehuang@g.harvard.edu>
2018-02-28 15:18:53 -0500

    new nodeversion update logic

--- benchmark/DB_index.hh
+++ benchmark/DB_index.hh
@@ -767,9 +767,20 @@ public:
             e->add_debug_text("inserted here");
             lp.value() = e;
 
-            auto orig_node = lp.node();
-            auto orig_nv = lp.previous_full_version_value();
-            auto new_nv = lp.next_full_version_value(1);
+            node_type *node;
+            nodeversion_value_type orig_nv;
+            nodeversion_value_type new_nv;
+
+            bool split_right = (lp.node() != lp.original_node());
+            if (split_right) {
+                node = lp.original_node();
+                orig_nv = lp.original_version_value();
+                new_nv = lp.updated_version_value();
+            } else {
+                node = lp.node();
+                orig_nv = lp.previous_full_version_value();
+                new_nv = lp.next_full_version_value(1);
+            }
 
             fence();
             lp.finish(1, *ti);
@@ -783,7 +794,15 @@ public:
             item.add_write();
             item.add_flags(insert_bit);
 
-            update_internode_version(orig_node, orig_nv, new_nv);
+            // add all newly created nodes to the read set
+            for (auto& np : lp.new_nodes()) {
+                if (!register_internode_version(np.first, np.second))
+                    goto abort;
+            }
+
+            // update the node version already in the read set and modified by split
+            if (!update_internode_version(node, orig_nv, new_nv))
+                goto abort;
         }
 
         return ins_return_type(true, found);
