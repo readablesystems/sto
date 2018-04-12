d63ad7562f16d904a07d428a955319d3f2e87e8a
Yihe Huang <yihehuang@g.harvard.edu>
2018-04-11 22:43:26 -0400

    comment out unused tables
    trying to speed up compile...

--- benchmark/Wikipedia_bench.hh
+++ benchmark/Wikipedia_bench.hh
@@ -40,41 +40,42 @@ public:
     template <typename K, typename V>
     using OIndex = ordered_index<K, V, DBParams>;
 
-    typedef OIndex<ipblocks_key, ipblocks_row>                           ipb_tbl_type;
-    typedef OIndex<ipblocks_addr_idx_key, ipblocks_addr_idx_row>         ipb_addr_idx_type;
-    typedef OIndex<ipblocks_user_idx_key, ipblocks_user_idx_row>         ipb_user_idx_type;
+    //typedef OIndex<ipblocks_key, ipblocks_row>                           ipb_tbl_type;
+    //typedef OIndex<ipblocks_addr_idx_key, ipblocks_addr_idx_row>         ipb_addr_idx_type;
+    //typedef OIndex<ipblocks_user_idx_key, ipblocks_user_idx_row>         ipb_user_idx_type;
     typedef OIndex<logging_key, logging_row>                             log_tbl_type;
     typedef OIndex<page_key, page_row>                                   page_tbl_type;
     typedef OIndex<page_idx_key, page_idx_row>                           page_idx_type;
-    typedef OIndex<page_restrictions_key, page_restrictions_row>         pr_tbl_type;
-    typedef OIndex<page_restrictions_idx_key, page_restrictions_idx_row> pr_idx_type;
+    //typedef OIndex<page_restrictions_key, page_restrictions_row>         pr_tbl_type;
+    //typedef OIndex<page_restrictions_idx_key, page_restrictions_idx_row> pr_idx_type;
     typedef OIndex<recentchanges_key, recentchanges_row>                 rc_tbl_type;
     typedef OIndex<revision_key, revision_row>                           rev_tbl_type;
     typedef OIndex<text_key, text_row>                                   text_tbl_type;
     typedef OIndex<useracct_key, useracct_row>                           user_tbl_type;
     typedef OIndex<useracct_idx_key, useracct_idx_row>                   user_idx_type;
-    typedef OIndex<user_groups_key, user_groups_row>                     ug_tbl_type;
+    //typedef OIndex<user_groups_key, user_groups_row>                     ug_tbl_type;
     typedef OIndex<watchlist_key, watchlist_row>                         wl_tbl_type;
     typedef OIndex<watchlist_idx_key, watchlist_idx_row>                 wl_idx_type;
 
     explicit wikipedia_db() :
-        tbl_ipb_(),
-        idx_ipb_addr_(),
-        idx_ipb_user_(),
+        //tbl_ipb_(),
+        //idx_ipb_addr_(),
+        //idx_ipb_user_(),
         tbl_log_(),
         tbl_page_(),
         idx_page_(),
-        tbl_pr_(),
-        idx_pr_(),
+        //tbl_pr_(),
+        //idx_pr_(),
         tbl_rc_(),
         tbl_rev_(),
         tbl_text_(),
         tbl_user_(),
         idx_user_(),
-        tbl_ug_(),
+        //tbl_ug_(),
         tbl_wl_(),
         idx_wl_() {}
 
+    /*
     ipb_tbl_type& tbl_ipblocks() {
         return tbl_ipb_;
     }
@@ -84,6 +85,7 @@ public:
     ipb_user_idx_type& idx_ipblocks_user() {
         return idx_ipb_user_;
     }
+    */
     log_tbl_type& tbl_logging() {
         return tbl_log_;
     }
@@ -93,12 +95,14 @@ public:
     page_idx_type& idx_page() {
         return idx_page_;
     }
+    /*
     pr_tbl_type& tbl_page_restrictions() {
         return tbl_pr_;
     }
     pr_idx_type& idx_page_restrictions() {
         return idx_pr_;
     }
+    */
     rc_tbl_type& tbl_recentchanges() {
         return tbl_rc_;
     }
@@ -114,9 +118,11 @@ public:
     user_idx_type& idx_useracct() {
         return idx_user_;
     }
+    /*
     ug_tbl_type& tbl_user_groups() {
         return tbl_ug_;
     }
+    */
     wl_tbl_type& tbl_watchlist() {
         return tbl_wl_;
     }
@@ -125,39 +131,39 @@ public:
     }
 
     void thread_init_all() {
-        tbl_ipb_.thread_init();
-        idx_ipb_addr_.thread_init();
-        idx_ipb_user_.thread_init();
+        //tbl_ipb_.thread_init();
+        //idx_ipb_addr_.thread_init();
+        //idx_ipb_user_.thread_init();
         tbl_log_.thread_init();
         tbl_page_.thread_init();
         idx_page_.thread_init();
-        tbl_pr_.thread_init();
-        idx_pr_.thread_init();
+        //tbl_pr_.thread_init();
+        //idx_pr_.thread_init();
         tbl_rc_.thread_init();
         tbl_rev_.thread_init();
         tbl_text_.thread_init();
         tbl_user_.thread_init();
         idx_user_.thread_init();
-        tbl_ug_.thread_init();
+        //tbl_ug_.thread_init();
         tbl_wl_.thread_init();
         idx_wl_.thread_init();
     }
 
 private:
-    ipb_tbl_type      tbl_ipb_;
-    ipb_addr_idx_type idx_ipb_addr_;
-    ipb_user_idx_type idx_ipb_user_;
+    //ipb_tbl_type      tbl_ipb_;
+    //ipb_addr_idx_type idx_ipb_addr_;
+    //ipb_user_idx_type idx_ipb_user_;
     log_tbl_type      tbl_log_;
     page_tbl_type     tbl_page_;
     page_idx_type     idx_page_;
-    pr_tbl_type       tbl_pr_;
-    pr_idx_type       idx_pr_;
+    //pr_tbl_type       tbl_pr_;
+    //pr_idx_type       idx_pr_;
     rc_tbl_type       tbl_rc_;
     rev_tbl_type      tbl_rev_;
     text_tbl_type     tbl_text_;
     user_tbl_type     tbl_user_;
     user_idx_type     idx_user_;
-    ug_tbl_type       tbl_ug_;
+    //ug_tbl_type       tbl_ug_;
     wl_tbl_type       tbl_wl_;
     wl_idx_type       idx_wl_;
 };
