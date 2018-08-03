ca32728d8f522d5126ba5f05b1bc4f7f1975bfe9
Zachary Yedidia <zyedidia@gmail.com>
2018-08-02 21:52:18 -0400

    Minor update to ycsb

--- benchmark/YCSB_bench.hh
+++ benchmark/YCSB_bench.hh
@@ -22,11 +22,13 @@ template <typename DBParams>
 class ycsb_db {
 public:
     template <typename K, typename V>
-    using OIndex = bench::art_index<K, V, DBParams>;
+    using OIndex = bench::ordered_index<K, V, DBParams>;
+    template <typename K, typename V>
+    using AIndex = bench::art_index<K, V, DBParams>;
     template <typename K, typename V>
     using UIndex = unordered_index<K, V, DBParams>;
 
-    typedef OIndex<ycsb_key, ycsb_value<DBParams>> ycsb_table_type;
+    typedef UIndex<ycsb_key, ycsb_value<DBParams>> ycsb_table_type;
 
     explicit ycsb_db() : ycsb_table_(ycsb_table_size) {}
 
@@ -35,7 +37,7 @@ public:
     }
 
     void table_thread_init() {
-        ycsb_table_.thread_init();
+        // ycsb_table_.thread_init();
     }
 
     void prepopulate();
