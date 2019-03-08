ccec215957835d1a8f60522eb2d70cef65033c4f
Yihe Huang <yihehuang@g.harvard.edu>
2019-03-08 13:54:38 -0500

    add huge page script

---
+++ mount_hugepages.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# Huge pages are 2MiB by default on our system (and this script can
+# only allocate huge pages in default sizes). Caculate the amount
+# of memory roughly needed, divide it by 2MiB, and that's the
+# number of pages to pass in to this script.
+
+# Common examples:
+
+# 64GiB = 32768 pages
+# 96GiB = 49152 pages <- use this one for TPC-C
+
+if [ $# -ge 1 ]; then
+  echo $1 | sudo tee /proc/sys/vm/nr_hugepages
+else
+  echo "Too few parameters. Exiting not doing anything."
+fi
