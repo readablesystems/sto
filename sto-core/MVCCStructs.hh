83d3e0324cfe536ef36d5d9a83857e62c5595406
William Qian <wqian@g.harvard.edu>
2020-12-06 17:03:35 -0500

    Checking for hvis status correctly

--- sto-core/MVCCStructs.hh
+++ sto-core/MVCCStructs.hh
@@ -664,7 +664,7 @@ public:
 
         // The previously-visible version for h
         history_type* hvis = h->prev();
-        assert(h->status_is(COMMITTED) || h->status_is(PENDING));
+        assert(hvis->status_is(COMMITTED) || hvis->status_is(PENDING));
 
         do {
             // Can only install onto the latest-visible version
