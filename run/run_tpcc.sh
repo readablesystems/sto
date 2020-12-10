5c0e95a79ed6f64d0464ef86acc9c3d96db379b7
William Qian <wqian@g.harvard.edu>
2020-12-10 13:44:08 -0500

    Updated experiment scripts

--- run/run_tpcc.sh
+++ run/run_tpcc.sh
@@ -3,5 +3,7 @@
 run/run_tpcc_occ.sh
 run/run_tpcc_tictoc.sh
 run/run_tpcc_mvcc.sh
-run/run_tpcc_mvcc_cu_past.sh
+#run/run_tpcc_mvcc_cu_past.sh
+run/run_tpcc_factors.sh
+run/run_tpcc_index_contention.sh
 sudo shutdown -h +1
--- run/run_wiki.sh
+++ run/run_wiki.sh
@@ -3,4 +3,8 @@
 run/run_wiki_occ.sh
 run/run_wiki_tictoc.sh
 run/run_wiki_mvcc.sh
+cd results
+join -t, --header --nocheck-order wiki_occ_results.txt wiki_tictoc_results.txt > /tmp/temp.txt
+join -t, --header --nocheck-order /tmp/temp.txt wiki_mvcc_results.txt > wiki_results.txt
+python3 /home/ubuntu/send_email.py --exp="Wikipedia" wiki_results.txt
 sudo shutdown -h +1
